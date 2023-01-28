76067e4401028f4d7564441c01580b1b61542540
Jeff Zhang <47371088+JeffDotPng@users.noreply.github.com>
2023-01-28 16:07:42 -0500

    challenge ui
    Co-authored-by: Auguste Rame <SuperAuguste@users.noreply.github.com>

---
+++ frontend/src/components/challenges/Challenge.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type ChallengeProps = {
+  description: string;
+  xp?: number;
+};
+
+export default function Challenge({ description, xp }: ChallengeProps) {
+  return (
+    <div className="border-black flex-1 p-4 border-b-2 last:border-b-0">
+      <div className="flex justify-between mb-2">
+        <div className="text-lg">{description}</div>
+        <div className="font-bold">{xp}xp</div>
+      </div>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ex
+        cumque voluptate eligendi unde beatae nesciunt laborum harum rem
+        similique quo sed nisi quae amet reiciendis eos, odio culpa natus?
+      </p>
+    </div>
+  );
+}
