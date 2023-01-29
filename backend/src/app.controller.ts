aca27b9142e744d5c0be4907dc44f4de9a5473d1
Joey Yu <joey@itsjoeoui.com>
2023-01-28 22:47:25 -0500

    Expose order
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post()
+  @Post('filldb')
   fillDb() {
     return this.appService.fillDb();
+  }
+
+  @Post('order')
+  order(@Param('userId') userId: number, @Param('orderId') orderId: number) {
+    return this.appService.order(userId, orderId);
   }
 }
